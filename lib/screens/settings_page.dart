4300611d54a45344d540472de9e08c9232a1c2cd
Ignacy Nowakowski <15818597+inowakowski@users.noreply.github.com>
2022-01-30 16:08:45 +0100

    visual changes in settings page

--- lib/screens/settings_page.dart
+++ lib/screens/settings_page.dart
@@ -1,9 +1,15 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:notes_app/db_helper/db_helper.dart';
 import 'package:azblob/azblob.dart';
+import 'package:notes_app/screens/login_page.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:notes_app/modal_class/settings.dart';
+import 'package:notes_app/modal_class/notes.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class SettingsPage extends StatefulWidget {
   final String appBarTitle;
@@ -28,15 +34,21 @@ class SettingsPageState extends State<SettingsPage> {
   String restoreDate;
   Color statusColor;
   Color restoreColor;
+  bool isLogIn = true;
   var user = 'user0';
   SettingsPageState(this.appBarTitle);
 
   @override
   Widget build(BuildContext context) {
     if (settingsList == null) {
+      // settingsList = ['', ''];
       settingsList = ['Not restored', 'Not synced'];
       updateSettingsView();
     }
+
+    final brightness = Theme.of(context).brightness;
+    bool isDarkMode = brightness == Brightness.dark;
+
     return WillPopScope(
         onWillPop: () async {
           moveToLastScreen();
@@ -55,7 +67,7 @@ class SettingsPageState extends State<SettingsPage> {
                   _saveSettings();
                 }),
           ),
-          body: Column(
+          body: ListView(
             children: <Widget>[
               Padding(
                 padding: EdgeInsets.all(20.0),
@@ -78,10 +90,10 @@ class SettingsPageState extends State<SettingsPage> {
                           height: 10.0,
                         ),
                         Text(
-                          settingsList[0] ??
-                              restoreDate ??
-                              settingsList[0]['restoreDate'] ??
-                              settings.restoreDate,
+                          restoreDate ??
+                              settingsList[0] ??
+                              settings.restoreDate ??
+                              settingsList[0]['restoreDate'],
                           style: TextStyle(color: restoreColor),
                           overflow: TextOverflow.ellipsis,
                         ),
@@ -91,23 +103,25 @@ class SettingsPageState extends State<SettingsPage> {
                   Padding(
                     padding: const EdgeInsets.all(12.0),
                     child: MaterialButton(
-                      onPressed: () {
-                        restoreFromAzure();
-                      },
+                      disabledColor: Colors.grey,
+                      onPressed: isLogIn
+                          ? () {
+                              restoreFromAzure();
+                            }
+                          : null,
                       child: Padding(
                         padding: const EdgeInsets.only(
-                            top: 8.0, bottom: 8.0, left: 20.0, right: 20.0),
+                            left: 20.0, right: 20.0, top: 5.0, bottom: 5.0),
                         child: Text(
                           'Restore',
                           style: Theme.of(context).textTheme.headline6,
                         ),
                       ),
                       color: Colors.blue,
-                      textColor: Colors.white,
                       padding: EdgeInsets.all(8.0),
                       splashColor: Colors.blueAccent,
                       shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(300.0)),
+                          borderRadius: BorderRadius.circular(25.0)),
                     ),
                   ),
                 ],
@@ -126,8 +140,8 @@ class SettingsPageState extends State<SettingsPage> {
                           height: 10.0,
                         ),
                         Text(
-                          settingsList[1] ??
-                              lastSyncDate ??
+                          lastSyncDate ??
+                              settingsList[1] ??
                               settingsList[1]['lastSyncDate'] ??
                               settings.lastSyncDate,
                           style: TextStyle(color: statusColor),
@@ -137,28 +151,86 @@ class SettingsPageState extends State<SettingsPage> {
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.all(20.0),
+                    padding: const EdgeInsets.all(12.0),
                     child: MaterialButton(
-                      onPressed: () {
-                        syncNotes();
-                      },
+                      disabledColor: Colors.grey,
+                      onPressed: isLogIn
+                          ? () {
+                              syncNotes();
+                            }
+                          : null,
                       child: Padding(
-                        padding: const EdgeInsets.all(8.0),
+                        padding: const EdgeInsets.only(
+                            left: 25.0, right: 25.0, top: 5.0, bottom: 5.0),
                         child: Text(
                           'Export',
                           style: Theme.of(context).textTheme.headline6,
                         ),
                       ),
                       color: Colors.blue,
-                      textColor: Colors.white,
-                      padding: EdgeInsets.all(12.0),
+                      padding: EdgeInsets.all(8.0),
                       splashColor: Colors.blueAccent,
                       shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(300.0)),
+                          borderRadius: BorderRadius.circular(25.0)),
                     ),
                   ),
                 ],
               ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: MaterialButton(
+                  onPressed: () {
+//LoginPage();
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => LogInPage("Log in")),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 40.0, right: 40.0, top: 5.0, bottom: 5.0),
+                    child: Text(
+                      isLogIn ? 'Log out' : 'Log in',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  ),
+                  color: isLogIn
+                      ? isDarkMode
+                          ? Colors.grey[850]
+                          : Colors.white
+                      : Colors.blue,
+                  padding: EdgeInsets.all(8.0),
+                  splashColor: Colors.blueAccent,
+                  shape: isLogIn
+                      ? RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25.0),
+                          side: BorderSide(color: Colors.blue, width: 2.0))
+                      : RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25.0)),
+                ),
+              ),
+              // Padding(
+              //   padding: const EdgeInsets.all(20.0),
+              //   child: MaterialButton(
+              //     onPressed: () {
+              //       helper.deleteAll();
+              //     },
+              //     child: Padding(
+              //       padding: const EdgeInsets.only(
+              //           left: 40.0, right: 40.0, top: 5.0, bottom: 5.0),
+              //       child: Text(
+              //         'Delete all notes',
+              //         style: Theme.of(context).textTheme.headline6,
+              //       ),
+              //     ),
+              //     color: Colors.blue,
+              //     padding: EdgeInsets.all(8.0),
+              //     splashColor: Colors.blueAccent,
+              //     shape: RoundedRectangleBorder(
+              //         borderRadius: BorderRadius.circular(25.0)),
+              //   ),
+              // ),
             ],
           ),
         ));
@@ -182,9 +254,10 @@ class SettingsPageState extends State<SettingsPage> {
 
       await storage.putBlob(
         '/$container/$fileName',
+        // '/$container/$user/$fileName',
         body: content,
       );
-      setState(() async {
+      setState(() {
         // settingsHelper.lastSyncDate =
         //     DateFormat.yMMMd().format(DateTime.now()) +
         //         ' ' +
@@ -195,10 +268,11 @@ class SettingsPageState extends State<SettingsPage> {
         //   await settingsHelper.insertSetiings(settings);
         // }
         lastSyncDate = DateFormat.yMMMd().format(DateTime.now()) +
-            ' ' +
+            '\n' +
             DateFormat.jms().format(DateTime.now());
         statusColor = Colors.green[600];
       });
+      // print('---------Export----------\n' + content);
     } on AzureStorageException catch (ex) {
       setState(() {
         lastSyncDate = 'Azure Storage Exception';
@@ -219,28 +293,40 @@ class SettingsPageState extends State<SettingsPage> {
   restoreFromAzure() async {
     try {
       String container = 'notter-project';
-      var storage = AzureStorage.parse(
-          'DefaultEndpointsProtocol=https;AccountName=notterprojectuser;AccountKey=ugCVzp4ihOOjoHtZzv9OhYbWpaeLl2Vv3hJ5Vt1y12e0I2NAxIQsSXelVE45Rm13UkwwKHJKT+9dIyh1TCYTHA==;EndpointSuffix=core.windows.net');
-      var body = storage.getBlob(
-        '/$container/notes.db',
-      );
 
-      print('------------------');
-      print('------------------');
-      print(body);
-      print('------------------');
-      setState(() async {
-        // var restoreStateDB = DateFormat.yMMMd().format(DateTime.now()) +
-        //     ' ' +
-        //     DateFormat.jms().format(DateTime.now());
+      var httpClient = http.Client();
+      var request = http.Request(
+          'GET',
+          Uri.parse(
+              'https://notterprojectuser.blob.core.windows.net/$container/$user//data/user/0/com.example.note_app_cdv/databases/notes.db'));
+      var response = await httpClient.send(request);
+      var body = await response.stream.bytesToString();
+      List<String> str = body
+          .replaceAll("}{", ";")
+          .replaceAll("{", "")
+          .replaceAll("}", "")
+          .split(",");
+      // Note note = Note.fromJson(jsonDecode(body));
+      // print(note.toMap());
+      print(str);
+      // print(note);
+      // await helper.deleteAll();
+      // for (int i = 0; i < contentList.length; i++) {
+      //   // await helper.insertNote(contentList[i]);
+      //   print(contentList[i]);
+      // }
+      setState(() {
+        var restoreStateDB = DateFormat.yMMMd().format(DateTime.now()) +
+            '\n' +
+            DateFormat.jms().format(DateTime.now());
         // settingsHelper.restoreDate = restoreStateDB;
-        // restoreDate = restoreStateDB;
+        restoreDate = restoreStateDB;
         // if (settings.id != null) {
         //   await settingsHelper.updateSettings(settings);
         // } else {
         //   await settingsHelper.insertSetiings(settings);
         // }
-        statusColor = Colors.green[600];
+        restoreColor = Colors.green[600];
       });
       restoreColor = Colors.green[600];
     } on AzureStorageException catch (ex) {
